a52c4c2ca5ea450bb173a4b7b6811b3e9d3e360d
Faizer Shaikh <faizershaikh25@gmail.com>
2025-12-31 18:45:02 +0530

    sadasd

--- server/src/server.js
+++ server/src/server.js
@@ -134,14 +134,20 @@ app.use('/socket.io/', (req, res, next) => {
   next();
 });
 
-// Test CORS endpoint
-app.options('*', (req, res) => {
-  const origin = req.headers.origin || '*';
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(200);
+// Handle OPTIONS requests for CORS preflight (Express 5 compatible)
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    const origin = req.headers.origin;
+    if (origin) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 // Setup Socket.io handlers
